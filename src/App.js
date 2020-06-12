fe553756fa8e973158165990145f34cfc1fb9ccb
usmangeeksquad <usmangeeksquad@gmail.com>
2020-06-12 13:56:49 +0500

    section4 nd section 5

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Section1 from '../src/components/section1/index'
 import Header from '../src/components/header/index'
 import Section2 from '../src/components/section2/index'
 import Section3 from '../src/components/section3/index'
+import Section4 from '../src/components/section4/section4'
+import Section5 from '../src/components/section5/section5'
+
 function App() {
   return (
     <>
-    <Header />
+    {/* <Header />
     <Section1 />
     <Section2 />
-    <Section3 />
+    <Section3 /> */}
+    <Section4/>
+    {/* <Section5/> */}
   </>
   );
 }
